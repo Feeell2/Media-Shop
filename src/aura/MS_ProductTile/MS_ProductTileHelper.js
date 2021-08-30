230947289a39bd32291847adca88a8af01a915d1
Feeell2 <tomek.filinski@gmail.com>
2021-08-30 14:16:49 +0200

    add spinner

---
+++ src/aura/MS_ProductTile/MS_ProductTileHelper.js
@@ -0,0 +1,47 @@
+({
+    addToCartHelper:function(component,event){
+    var action = component.get('c.addProductToCache');
+            action.setParams({
+              product:component.get('v.product'),
+              quantity:"1",
+            })
+
+            action.setCallback(this, function(response) {
+             let state=response.getState();
+             console.log(state);
+             let result = response.getReturnValue();
+             this.showToast(component,event,"Added To Cart","success");
+             this.checkAddingToCart(component,event);
+            })
+            $A.enqueueAction(action);
+    },
+
+    getProductsToListHelper:function(component,event){
+    let action = component.get('c.getProductsToCart');
+          action.setCallback(this, function(response) {
+            let state=response.getState();
+            let result = response.getReturnValue();
+            if(state==='SUCCESS'){
+                component.set('v.listProducts',result.products);
+                this.checkAddingToCart(component,event);
+            }
+          })
+          $A.enqueueAction(action);
+    },
+
+    checkAddingToCart:function(component,event){
+        let listOfProduct=component.get('v.listProducts');
+        let id=component.get('v.product.Product2Id');
+        for(let i=0;i<listOfProduct.length;i++){
+          if(listOfProduct[i].product.Product2Id==id){
+              component.set('v.disableAddToCart',true);
+          }
+        }
+    },
+    showToast : function(component, event,message,typeToast) {
+          component.set("v.type", typeToast);
+          component.set("v.message", message);
+          let childComponent = component.find("toastComponentProduct");
+          let fireToast = childComponent.toast();
+      },
+})
