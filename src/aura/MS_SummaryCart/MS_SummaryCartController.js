66055521268ba78cb924564d2c6f0e7baf88fde3
Feeell2 <tomek.filinski@gmail.com>
2021-09-05 13:05:16 +0200

    fix move to order page

--- src/aura/MS_SummaryCart/MS_SummaryCartController.js
+++ src/aura/MS_SummaryCart/MS_SummaryCartController.js
@@ -3,4 +3,12 @@
         helper.initDo(component,event);
     },
 
+    navigate : function(component, event, helper) {
+               let address = '/summaryorder';
+               let urlEvent = $A.get("e.force:navigateToURL");
+               urlEvent.setParams({
+                 "url": address,
+               });
+               urlEvent.fire();
+    },
 })
